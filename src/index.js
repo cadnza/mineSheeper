2d8fd189a7acecd37b3828eec266f06addfe6ba9
Jon Dayley <jonathandayley@gmail.com>
2021-06-07 15:33:07 -0600

    Write constructor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Board extends React.Component {
-	constructor(){
-	}
+	constructor() {
+		super()
+		this.state = {
+			size: [10,10],
+			options: {
+				Easy: [10,10],
+				Medium: [16,16],
+				Hard: [24,30]
+			}
+		};
+	}	
 }
