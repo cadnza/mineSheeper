c59cafc24490e5e8199a41152ec192e5d368e1b4
Jon Dayley <jonathandayley@gmail.com>
2021-06-09 14:34:37 -0600

    Rename file with difficulty class to controller pending class rename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Difficulty from "./difficulty.js";
+import Difficulty from "./controller.js";
 
 ReactDOM.render(<Difficulty />,document.getElementById("game"));
