50244d8cea744c93f83270c8b43acf73e9d40b38
Jon Dayley <jonathandayley@gmail.com>
2021-06-09 15:42:36 -0600

    Update references

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Controller from "./controller.js";
+import Controller from "./Controller.js";
 
 ReactDOM.render(<Controller />,document.getElementById("game"));
