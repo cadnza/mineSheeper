f07f83269fc1be9027cf61bfe3476933eac26afd
Jon Dayley <jonathandayley@gmail.com>
2021-06-09 13:27:19 -0600

    Remove redundant import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Difficulty from "./difficulty.js";
-import Minefield from "./minefield.js";
 
 ReactDOM.render(<Difficulty />,document.getElementById("game"));
