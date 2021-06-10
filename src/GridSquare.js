7b877c0a317cf96459406f12334584530d557381
Jon Dayley <jonathandayley@gmail.com>
2021-06-10 16:21:15 -0600

    Fix reset game method to prevent unnecessary refresh

--- src/GridSquare.js
+++ src/GridSquare.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 class GridSquare extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			clicked: false
+		};
+	}
 	render() {
 		const final = <div>
 			<button
 				className="mineButton"
 				id={this.props.btnId}
 				onClick={() => {
+					this.setState({clicked: true});
 					this.props.clickHandler();
 				}}
 			>
@@ -15,11 +22,12 @@ class GridSquare extends React.Component {
 		</div>;
 		return final;
 	}
-	reset() {
+	reset = () => {
 		const btnRef = document.getElementById(this.props.btnId);
 		btnRef.innerText = this.props.unclickedText;
 		btnRef.disabled = false;
-	}
+		this.setState({clicked: false});
+	};
 }
 
 export default GridSquare;
--- src/Minefield.js
+++ src/Minefield.js
@@ -14,6 +14,7 @@ class Minefield extends React.Component {
 	}
 	render() {
 		const final = this.buildUI();
+		this.resetGame();
 		return final;
 	}
 	// Set method to build UI
@@ -36,8 +37,6 @@ class Minefield extends React.Component {
 				/>
 			);
 		}
-		// Reset all mine buttons (in case any have already been clicked)
-		Object.keys(this.refs).map((x) => {return this.refs[x].reset();});
 		// Set mine buttons into div
 		var final = <div
 			id="mines"
@@ -51,6 +50,18 @@ class Minefield extends React.Component {
 		</div>;
 		// Return
 		return final;
+	};
+	resetGame = () => {
+		// Check whether reset is necessary
+		const allButtonRefs = Object.keys(this.refs).filter(
+			x => {return x.match("^" + this.state.btnClassIdPrefix);}
+		).map(x => {return this.refs[x];});
+		const gridReady = allButtonRefs.map(x => {return x.state.clicked;}).indexOf(true) === -1;
+		// Perform reset if necessary
+		if(!gridReady) {
+			// Reset individual buttons
+			allButtonRefs.map(x => {return x.reset();});
+		}
 	};
 	processSquareClick = (idx) => {
 		if(!this.state.started) {
@@ -68,14 +79,14 @@ class Minefield extends React.Component {
 		// Get grid array length
 		const gridLen = this.props.size[0] * this.props.size[1];
 		// Get options for mine placement
-		var minePlacementOpts = Array.from(Array(gridLen).keys()).filter((x) => {return x !== idxFirstClicked;});
+		var minePlacementOpts = Array.from(Array(gridLen).keys()).filter(x => {return x !== idxFirstClicked;});
 		// Sample placement options
 		var placements = [];
 		for(var i = 0; i < this.props.nMines; i++) {
 			const nextMine = minePlacementOpts[
 				Math.floor(Math.random() * minePlacementOpts.length)
 			];
-			minePlacementOpts = minePlacementOpts.filter((x) => {return x !== nextMine;});
+			minePlacementOpts = minePlacementOpts.filter(x => {return x !== nextMine;});
 			placements.push(nextMine);
 		}
 		placements = placements.sort((a,b) => {return a - b;});
@@ -88,7 +99,7 @@ class Minefield extends React.Component {
 		// Add counts to non-mine fields in hidden array
 		for(i = 0; i < arrHidden.length; i++) {
 			if(typeof arrHidden[i] === "undefined") {
-				const ctMines = this.getAdjSquares(i).filter((x) => {return x === this.kMine;}).length;
+				const ctMines = this.getAdjSquares(i).filter(x => {return x === this.kMine;}).length;
 				arrHidden[i] = ctMines;
 			}
 		}
@@ -139,7 +150,7 @@ class Minefield extends React.Component {
 			final.push(idx + this.props.size[0] + 1);
 		}
 		// Convert back to 0 basis
-		final = final.map((x) => {return x - 1;});
+		final = final.map(x => {return x - 1;});
 		// Return
 		return final;
 	};
