2d6fcdde32ab08a35477e1f9687dda3aca5cabfa
Jon Dayley <jonathandayley@gmail.com>
2021-07-06 07:45:09 -0600

    Add fix for carryover muting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 
 import Controller from "./Controller.js";
 
+document.getElementById("music").muted = false;
+//document.getElementById("sound").muted = false; //TEMP
+
 ReactDOM.render(<Controller />,document.getElementById("game"));
